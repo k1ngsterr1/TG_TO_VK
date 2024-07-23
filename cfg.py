17ff171e0b2d3f2d79acaaeb6cd5f7f7a87ad935
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-07-23 12:58:07 +0500

    --TG TO VK UPDATED

--- cfg.py
+++ cfg.py
@@ -1,6 +1,9 @@
+import os
 # TOKEN = "7083290681:AAEMok7bOyqUbBMXyBOSH5qoufzsHVaGGws"
 # CHANNEL = "@polevoytg"
 # CHANNEL_LINK = "https://t.me/+--kocMZbBiRkMjcy"
 
 # DIR_RELATIONS = "/usr/src/app/relations.json"
-DIR_RELATIONS = "/usr/src/app/relations.json"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# DIR_RELATIONS = "/home/k1ngsterr/Desktop/Projects/telegram_bot_backend/bots/TG_TO_VK/relations.json"
+DIR_RELATIONS = os.path.join(BASE_DIR, 'relations.json')
